70fb276f1f08ea2f39ed93b2adcc6590a661553b
ThePlenkov <petr.plenkov@gmail.com>
2022-07-18 08:40:57 +0000

    ready for use

---
+++ bin/check.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+import { argv, env, exit } from "node:process";
+import assert from "node:assert";
+
+try {
+  // check arguments
+  argv.splice(2).forEach((arg) => {
+    // equals condition
+    const [key, value] = arg.split("=");
+    key && assert.equal(env[key], value);
+  });
+} catch (error) {
+  // exit process
+  exit(1);
+}
